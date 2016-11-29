f3fc0641927e197fdc4f7fc02092b2d4545327b7
Niko Lepka <slench102+git@gmail.com>
2016-11-29 15:55:47 +0100

    adding the files

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"			// imports command-line args
+	"fmt"			// imports Printf and cousins
+	"encoding/json"		// imports JSON utilities
+	"io/ioutil"		// imports file utilities
+)
+
+type InputData struct {
+	Colors []string `json:"colors"`
+	Sizes  []int    `json:"sizes"`
+	Amount int      `json:"amount"`
+}
+
+func main() {
+	var inputFileName, outputFileName string
+	var parsedData InputData
+
+	arguments := os.Args[1:] // arguments without program name
+
+	if len(arguments) < 1 {
+		panic("Please supply a generator file")
+	}
+
+	inputFileName = arguments[0] // The JSON file we need to read
+
+	inputFile, readErr := ioutil.ReadFile(inputFileName)
+
+	if readErr != nil {
+		// add code
+	}
+
+	jsErr := json.Unmarshal(inputFile, &parsedData)
+
+	if jsErr != nil {
+		// add code
+	}
+	
+	var in, out InputData
+	var b []byte
+	
+	in = InputData {
+		Colors: []string{"0xFF0000", "0x00FF00", "0x0000FF"},
+		Sizes: []int{50, 100, 150},
+		Amount: 500,
+	}
+
+	b, err = json.Marshal(in)
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(b))
+	
+	err = json.Unmarshal(b, &out)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	fmt.Println(out)
+}
