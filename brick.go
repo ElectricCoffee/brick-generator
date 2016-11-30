1951b158e707f9a2aaddd63b32fb2a45d25ea898
electric coffee <slench102@gmail.com>
2016-11-30 21:17:00 +0100

    add FWriteDataSet

--- brick.go
+++ brick.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
 
 type Brick struct {
 	Colour uint
@@ -15,3 +19,14 @@ func (b Brick) String() string {
 	return fmt.Sprintf("COL:%d LEN:%d\n", b.Colour, b.Size)
 }
 
+func FWriteDataSet(filename string, input []Brick) error {
+	strArr := make([]string, len(input))
+
+	for i, brick := range input {
+		strArr[i] = brick.String()
+	}
+
+	byteArr := []byte(strings.Join(strArr, ""))
+	// 0644 is the unix permission that sets -rw-r--r--
+	return ioutil.WriteFile(filename, byteArr , 0644)
+}
